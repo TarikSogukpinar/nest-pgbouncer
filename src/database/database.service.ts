74edd084ef5726ddb114608301d0d642fbde69e2
TarıkSogukpinar <tariksogukpinar@outlook.com>
2024-12-27 22:29:26 +0300

    first

---
+++ src/database/database.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy {
+    constructor(private configService: ConfigService) {
+        const prisma = new PrismaClient();
+
+        super({
+            datasources: {
+                db: {
+                    url: configService.get<string>('DATABASE_URL'),
+                },
+            },
+        });
+
+        Object.assign(this, prisma);
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
